2894490d8ad9c8a65fa2ecf0146c55d05ed7407d
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2026-04-04 23:52:58 +0500

    Write code to select toolBtn add eventListener to canvas mousedown, mouseup, mousemove and make related functions

--- script.js
+++ script.js
@@ -19,3 +19,40 @@ let prevMouseX,
   selectedTool = "pencil",
   brushWidth = 5,
   selectedColor = "#000";
+
+
+const setCanvasBackground = () => {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = selectedColor;
+};
+
+
+window.addEventListener("load", () => {
+  // Setting canvas widht/height..
+  // offsetwidht/height returns
+  // viewbale widht/height of an element
+
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+  setCanvasBackground();
+});
+
+toolBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".options .active").classList.remove("active");
+    btn.classList.add("active");
+    selectedTool = btn.id;
+    console.log(selectedTool);
+  });
+});
+
+
+canvas.addEventListener("mousedown",startDraw);
+canvas.addEventListener("mousemove",drawing);
+canvas.addEventListener("mouseup",() => (isDrawing = false), saveState());
+
+const startDraw = (e) => {};
+const drawing = (e) => {};
+
+function saveState(){}
